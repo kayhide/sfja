987d83bf279ceecd81a2e8fd5bd1b7e8aa9802ec
kayhide <kayhide@gmail.com>
2018-09-04 23:52:50 +0900

    update list_funs proofs.

--- Lists_J.v
+++ Lists_J.v
@@ -341,7 +341,7 @@ Fixpoint nonzeros (l:natlist) : natlist :=
   end.
 
 Example test_nonzeros:            nonzeros [0,1,0,2,3,0,0] = [1,2,3].
-Proof. simpl. reflexivity. Qed.
+Proof. reflexivity. Qed.
 
 Fixpoint oddmembers (l:natlist) : natlist :=
   match l with
@@ -350,17 +350,17 @@ Fixpoint oddmembers (l:natlist) : natlist :=
   end.
 
 Example test_oddmembers:            oddmembers [0,1,0,2,3,0,0] = [1,3].
-Proof. simpl. reflexivity. Qed.
+Proof. reflexivity. Qed.
 
 Fixpoint countoddmembers (l:natlist) : nat :=
   length (oddmembers l).
 
 Example test_countoddmembers1:    countoddmembers [1,0,3,1,4,5] = 4.
-Proof. simpl. reflexivity. Qed.
+Proof. reflexivity. Qed.
 Example test_countoddmembers2:    countoddmembers [0,2,4] = 0.
-Proof. simpl. reflexivity. Qed.
+Proof. reflexivity. Qed.
 Example test_countoddmembers3:    countoddmembers nil = 0.
-Proof. simpl. reflexivity. Qed.
+Proof. reflexivity. Qed.
 (** [] *)
 
 (* **** Exercise: 2 stars (alternate) *)
