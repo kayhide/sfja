8b7304c60205bb43b7ebb7d055851cbb25ca8771
kayhide <kayhide@gmail.com>
2018-09-18 22:30:44 +0900

    solve override_shadow of Poly.

--- Poly_J.v
+++ Poly_J.v
@@ -1098,7 +1098,13 @@ Proof.
 Theorem override_shadow : forall {X:Type} x1 x2 k1 k2 (f : nat->X),
   (override (override f k1 x2) k1 x1) k2 = (override f k1 x1) k2.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros X x1 x2 k1 k2 f.
+  unfold override.
+
+  destruct (beq_nat k1 k2).
+  reflexivity.
+  reflexivity.
+Qed.
 (** [] *)
 
 (** **** 練習問題: ★★★, recommended (combine_split) *)
