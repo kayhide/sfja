aa1d675200371aeee55bf00734279171a9b51fef
kayhide <kayhide@gmail.com>
2018-12-02 14:54:22 +0900

    solve ev_sum of Prop.

--- Prop_J.v
+++ Prop_J.v
@@ -1395,7 +1395,15 @@ Proof.
 Theorem ev_sum : forall n m,
    ev n -> ev m -> ev (n+m).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m E1 E2.
+  induction E1.
+  simpl.
+  apply E2.
+
+  simpl.
+  apply ev_SS.
+  apply IHE1.
+Qed.
 (** [] *)
 
 
